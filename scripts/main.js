d71c3340d39d2da043631389e7168cd52a44ebb4
Saurav Kumar <sauravk7077@gmail.com>
2020-11-20 11:06:44 +0530

    added shops

--- scripts/main.js
+++ scripts/main.js
@@ -20,7 +20,7 @@ function init() {
             props.biscuitCount += x.value * x.count;
         }
         display();
-    }, 1);
+    }, 100);
 }
 function biscuitClick() {
     props.biscuitCount += props.clickValue;
@@ -28,6 +28,49 @@ function biscuitClick() {
 }
 function display() {
     biscuitCountDiv.innerText = props.biscuitCount.toString();
+    var shopContainer = document.getElementById('shopsContainer');
+    if (shopContainer) {
+        var shopBtn = shopContainer.children[0];
+        for (var _i = 0, _a = props.biscuitValues; _i < _a.length; _i++) {
+            var x = _a[_i];
+            shopBtn.children[1].children[0].children[0].innerText = x.cost;
+            shopBtn.children[1].children[1].children[0].innerText = x.count;
+        }
+    }
     dataStorage.set(props);
+}
+function formatName(s) {
+    return s.replace(/\w\S*/g, function (txt) {
+        return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();
+    })
+        .replace(/ /g, '');
+}
+function handleShopClick(e, id) {
+    var currentEl = props.biscuitValues.filter(function (el) { return el.name === id; })[0];
+    if (props.biscuitCount >= currentEl.cost) {
+        currentEl.count++;
+        props.biscuitCount -= currentEl.cost;
+    }
+    display();
+}
+function generateShops() {
+    var htmlCode = "\n    <div class=\"shopBtn\">\n        <div class=\"icon\"></div>\n        <div>Clicker</div>\n    </div>\n    <div>\n        <div>Cost: <span></span></div>\n        <div>Total: <span></span></div>\n    </div>\n";
+    var containerDiv = document.createElement("div");
+    containerDiv.id = "shopsContainer";
+    var _loop_1 = function (x) {
+        var shopBtn = document.createElement("div");
+        shopBtn.className = "shopContent";
+        shopBtn.id = formatName(x.name);
+        shopBtn.innerHTML = htmlCode;
+        shopBtn.children[0].addEventListener('click', function (e) { handleShopClick(e, shopBtn.id); });
+        shopBtn.children[1].children[0].children[0].innerText = x.cost;
+        shopBtn.children[1].children[1].children[0].innerText = x.count;
+        containerDiv.appendChild(shopBtn);
+    };
+    for (var _i = 0, _a = props.biscuitValues; _i < _a.length; _i++) {
+        var x = _a[_i];
+        _loop_1(x);
+    }
+    document.body.append(containerDiv);
 }
 init();
--- tsScripts/main.ts
+++ tsScripts/main.ts
@@ -21,7 +21,7 @@ function init() {
             props.biscuitCount += x.value * x.count;
         }
         display();
-    },1);
+    },100);
 }
 
 
@@ -32,7 +32,63 @@ function biscuitClick() {
 
 function display() {
     biscuitCountDiv.innerText = props.biscuitCount.toString();
+    const shopContainer = document.getElementById('shopsContainer');
+    if(shopContainer){
+        const shopBtn = shopContainer.children[0];
+        for(let x of props.biscuitValues){
+            shopBtn.children[1].children[0].children[0].innerText = x.cost;
+            shopBtn.children[1].children[1].children[0].innerText = x.count;
+        }
+    }
+    
+
     dataStorage.set(props);
+}
+
+function formatName(s: string) {
+    return s.replace(
+        /\w\S*/g,
+        function(txt) {
+          return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();
+        }
+      )
+      .replace(/ /g,'');
+    
+}
+function handleShopClick(e:Event, id:string) {
+    const currentEl = props.biscuitValues.filter(el=> el.name === id)[0];
+    if(props.biscuitCount >= currentEl.cost){
+        currentEl.count++;
+        props.biscuitCount -= currentEl.cost;
+    }
+    display();
+}
+
+function generateShops() {
+    const htmlCode = `
+    <div class="shopBtn">
+        <div class="icon"></div>
+        <div>Clicker</div>
+    </div>
+    <div>
+        <div>Cost: <span></span></div>
+        <div>Total: <span></span></div>
+    </div>
+`;
+    const containerDiv = document.createElement("div");
+    containerDiv.id = "shopsContainer";
+    for(let x of props.biscuitValues){
+        const shopBtn = document.createElement("div");
+        shopBtn.className = "shopContent";
+        shopBtn.id =  formatName(x.name);
+        shopBtn.innerHTML = htmlCode;
+        shopBtn.children[0].addEventListener('click', e => {handleShopClick(e, shopBtn.id)});
+        shopBtn.children[1].children[0].children[0].innerText = x.cost;
+        shopBtn.children[1].children[1].children[0].innerText = x.count;
+        containerDiv.appendChild(shopBtn);
+    }
+    document.body.append(containerDiv);
+
 }
 
 
